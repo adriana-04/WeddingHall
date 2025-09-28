3d2be09830b55598acbd27504ae7dcdaa3bab8a9
Adawiyah Hashim <adrianaalia0404@gmail.com>
2025-09-28 23:19:24 +0800

    add users blueprint table and migrate

--- database/migrations/2025_09_28_151128_create_users_table.php
+++ database/migrations/2025_09_28_151128_create_users_table.php
@@ -9,13 +9,19 @@ return new class extends Migration
     /**
      * Run the migrations.
      */
-    public function up(): void
-    {
-        Schema::create('users', function (Blueprint $table) {
-            $table->id();
-            $table->timestamps();
-        });
-    }
+    public function up()
+{
+    Schema::create('users', function (Blueprint $table) {
+        $table->id();
+        $table->string('full_name');
+        $table->string('last_name');
+        $table->string('username')->unique();
+        $table->string('email')->unique();
+        $table->string('password');
+        $table->timestamps();
+    });
+}
+
 
     /**
      * Reverse the migrations.
